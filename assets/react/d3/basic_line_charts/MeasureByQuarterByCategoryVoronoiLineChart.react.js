528589b77037c8b483932aa41dd8763c99442cb4
Paul Tomchik <pauljtomchik@gmail.com>
2015-06-24 15:39:54 -0400

    Fixed vornoi text area so that it stays in the chart's area and does not get clipped.

--- assets/react/d3/basic_line_charts/MeasureByQuarterByCategoryVoronoiLineChart.react.js
+++ assets/react/d3/basic_line_charts/MeasureByQuarterByCategoryVoronoiLineChart.react.js
@@ -1,6 +1,10 @@
 /*jshint strict:false , unused:false */
 /*globals $ */
 
+/* This file needs a major refactor, but the heavy use of closures
+ * means that breaking the code into smaller functions may result
+ * in even less readable code. 
+ */                                   
 
 
 var React                 = require('react'),
@@ -30,43 +34,45 @@ var MeasureByQuarterByGeographyVoronoiLineChart = React.createClass({
 
     '_update' : function () {
 
-        var props    = this.props,
+        var that     = this,
+
+            props    = this.props,
             data     = props.data || [],
             measure  = props.measure,
             category = props.category,
+
             svgNode  = React.findDOMNode(this.refs.theSVG),
             theSVG   = d3.select(svgNode),
-            width    = svgNode.offsetWidth,
-            that     = this,
             theG,
             categoriesG,
+
+            height   = props.height,
+            width    = svgNode.offsetWidth,
+            margin   = props.margin,
+
             subcategoriesDataObj,
             categoriesVoronoiObj,
             categoriesArr;
 
-        var quarterToMonth = { '1': '02', '2': '05', '3': '08', '4': '11' };
-        
-        console.log('=======================');
-        console.log(theSVG);
-        console.log(props.margin);
-        console.log([[ -props.margin.left, -props.margin.top], 
-                         [ width + props.margin.right, props.height + props.margin.bottom]]);
-        console.log('=======================');
 
+        var quarterToMonth = { '1': '02', '2': '05', '3': '08', '4': '11' };
+
+        
         var voronoi = d3.geom.voronoi()
-            .x(function(d) { return that._x(d.date); })
-            .y(function(d) { return that._y(d[measure]); })
-            .clipExtent([[ -props.margin.left, -props.margin.top], 
-                         [ width + props.margin.right, props.height + props.margin.bottom]]);
+                             .x(function(d) { return that._x(d.date); })
+                             .y(function(d) { return that._y(d[measure]); })
+                             .clipExtent([[ -margin.left, -margin.top], 
+                                          [ width + margin.right, height + margin.bottom]]);
 
         var color = d3.scale.category20();
 
+        
         // Clear the Visualization.
         // TODO: Try to just remove the axes, then use `exit` on the chart.
         theSVG.selectAll('*').remove();
 
 
-        // Parse the data
+        // Parse the data.
         subcategoriesDataObj = {};
         data.forEach(function(d) {
 
@@ -92,6 +98,14 @@ var MeasureByQuarterByGeographyVoronoiLineChart = React.createClass({
         });
 
 
+        color.domain(Object.keys(subcategoriesDataObj));
+
+        this._x.domain(d3.extent(data, function(d) { return d.date;     }));
+        this._y.domain(d3.extent(data, function(d) { return d[measure]; }));
+
+
+        // Turn the parsed data into an array of data
+        // structured for the voronoi algorithm.
         categoriesArr = Object.keys(subcategoriesDataObj).map(function (subcategory) {
             var data = { 
                 subcategory : subcategory ,
@@ -108,16 +122,12 @@ var MeasureByQuarterByGeographyVoronoiLineChart = React.createClass({
         });
 
 
-        color.domain(Object.keys(subcategoriesDataObj));
-
-        this._x.domain(d3.extent(data, function(d) { return d.date;     }));
-        this._y.domain(d3.extent(data, function(d) { return d[measure]; }));
-
 
         theG = theSVG.append('g')
                      .style('width',    width)
-                     .style('height',   props.height - props.margin.top - props.margin.bottom)
-                     .attr('transform', 'translate(' + props.margin.left + ',' + props.margin.top + ')');
+                     .style('height',   height - margin.top - margin.bottom)
+                     .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
+
 
         categoriesG = theG.selectAll('.category')
                           .data(categoriesArr)
@@ -132,95 +142,104 @@ var MeasureByQuarterByGeographyVoronoiLineChart = React.createClass({
             .style('stroke', function(d) { return color(d.subcategory); });
 
         categoriesG.append('text')
-            .datum(function (d) { return { subcategory : d.subcategory,
-                                           label       : props.category_labels[d.subcategory],
-                                           value       : d.values[d.values.length -1] }; })
+                   .datum(function (d) { return { subcategory : d.subcategory,
+                                                  label       : props.category_labels[d.subcategory],
+                                                  value       : d.values[d.values.length -1] }; })
 
-            .attr('transform', function(d) { return 'translate('                +
-                                                    that._x(d.value.date)       +
-                                                    ','                         +
-                                                    that._y(d.value[measure]) +
-                                                    ')'; })
-            .attr('x', 3)
-            .attr('dy', '0.35em')
-            .attr('class', 'exportable')
-            .text(function (d) { return d.label; })
-            .style('font-size', '10px')
-            .style('fill', function(d) { return color(d.subcategory); });
-
-
-
-  var focus = theG.append("g")
-      .attr("transform", "translate(-100,-100)")
-      .attr("class", "focus");
-
-  focus.append("circle")
-      .attr("r", 3.5);
-
-  focus.append("text")
-      .attr("y", -10);
+                   .attr('transform', function(d) { return 'translate('                +
+                                                           that._x(d.value.date)       +
+                                                           ','                         +
+                                                           that._y(d.value[measure]) +
+                                                           ')'; })
+                   .attr('x', 3)
+                   .attr('dy', '0.35em')
+                   .attr('class', 'exportable')
+                   .text(function (d) { return d.label; })
+                   .style('font-size', '10px')
+                   .style('fill', function(d) { return color(d.subcategory); });
 
 
-  var voronoiGroup = theG.append("g")
-      .attr("class", "voronoi");
 
-  voronoiGroup.selectAll("path")
-    .data(voronoi(d3.nest()
-                      .key(function (d) { return that._x(d.date) + "," + that._y(d[measure]); })
-                      .rollup(function (v) { return v[0]; })
-                      .entries(d3.merge(categoriesArr.map(function (d) { return d.values; })))
-                      .map(function(d) { return d.values; })))
-    .enter().append("path")
-            .attr("d", function(d) { return "M" + d.join("L") + "Z"; })
-            .datum(function(d) { return d.point; })
-            .on("mouseover", mouseover)
-            .on("mouseout", mouseout);
+        var focus = theG.append("g")
+                    .attr("transform", "translate(-100,-100)")
+                    .attr("class", "focus");
 
-  d3.select("#show-voronoi")
-      .property("disabled", false)
-      .on("change", function() { console.log('Sup'); voronoiGroup.classed("voronoi--show", this.checked); });
+        focus.append("circle")
+            .attr("r", 3.5);
 
-  function mouseover(d) {
-    var textNode = focus.select('text');
+        focus.append("text")
+             .attr("y", -10);
 
-    d3.select(d.circularRef.line)
-      .classed("line--hover", true);
 
-    d.circularRef.line.parentNode.appendChild(d.circularRef.line);
+        var voronoiGroup = theG.append("g")
+                               .attr("class", "voronoi");
 
-    focus.attr("transform", "translate(" + that._x(d.date) + "," + that._y(d[measure]) + ")");
+        voronoiGroup.selectAll("path")
+                    .data(voronoi(d3.nest()
+                                    .key(function (d) { return that._x(d.date) + "," + that._y(d[measure]); })
+                                    .rollup(function (v) { return v[0]; })
+                                    .entries(d3.merge(categoriesArr.map(function (d) { return d.values; })))
+                                    .map(function(d) { return d.values; })))
+                    .enter().append("path")
+                            .attr("d", function(d) { return "M" + d.join("L") + "Z"; })
+                            .datum(function(d) { return d.point; })
+                            .on("mouseover", mouseover)
+                            .on("mouseout", mouseout);
 
-    textNode.selectAll('*').remove();
+        d3.select("#show-voronoi")
+          .property("disabled", false)
+          .on("change", function() { console.log('Sup'); voronoiGroup.classed("voronoi--show", this.checked); });
 
-    textNode.style('text-shadow', '5px 5px 5px #000, 5px 5px 5px #000, 5px 5px 5px black, 5px 5px 5px black')
-        .style('font-size', '20px')
-        .style('stroke', color(d.circularRef.subcategory) )
-        .style('fill', 'white');
 
-    textNode.append('tspan')
-        .attr('x', 0)
-        .attr('dy', '1.2em')
-        .text(props.category_labels[d.circularRef.subcategory]);
-        
-    textNode.append('tspan')
-        .attr('x', 0)
-        .attr('dy', '1.2em')
-        .text(d.year + ' Q' + d.quarter);
+        function mouseover(d) {
+            var textNode       = focus.select('text'),
+                textNodeHeight = textNode.node().getBBox().height,
+                pointYCoord    = that._y(d[measure]),
 
-    textNode.append('tspan')
-        .attr('x', 0)
-        .attr('dy', '1.2em')
-        .text(measure + ': ' + d[measure]);
-  }
+                // Make sure the text node is within the chart.
+                textNodeYCoord = ((pointYCoord - textNodeHeight) > 0) ? 
+                                    (pointYCoord - textNodeHeight)    : 
+                                    pointYCoord;
 
-  function mouseout(d) {
-      console.log('mouseout');
-      console.log(d);
-    d3.select(d.circularRef.line)
-        .classed("line--hover", false);
+            d3.select(d.circularRef.line)
+              .classed("line--hover", true);
 
-    focus.attr("transform", "translate(-100,-100)");
-  }
+            d.circularRef.line.parentNode.appendChild(d.circularRef.line);
+
+            focus.attr("transform", "translate(" + that._x(d.date) + "," + textNodeYCoord + ")");
+
+            textNode.selectAll('*').remove();
+
+            textNode.style('text-shadow', '5px 5px 5px black,' +
+                                          '5px 5px 5px black,' +
+                                          '5px 5px 5px black,' +
+                                          '5px 5px 5px black'  )
+                    .style('font-size', '20px')
+                    .style('stroke', color(d.circularRef.subcategory) )
+                    .style('fill', 'white');
+
+            textNode.append('tspan')
+                .attr('x', 0)
+                .attr('dy', '1.2em')
+                .text(props.category_labels[d.circularRef.subcategory]);
+                
+            textNode.append('tspan')
+                .attr('x', 0)
+                .attr('dy', '1.2em')
+                .text(d.year + ' Q' + d.quarter);
+
+            textNode.append('tspan')
+                .attr('x', 0)
+                .attr('dy', '1.2em')
+                .text(measure + ': ' + d[measure]);
+        }
+
+        function mouseout(d) {
+            d3.select(d.circularRef.line)
+              .classed("line--hover", false);
+
+            focus.attr("transform", "translate(-100,-100)");
+        }
 
         theG.append('g')
             .attr('class', 'x axis')
@@ -246,9 +265,11 @@ var MeasureByQuarterByGeographyVoronoiLineChart = React.createClass({
     },
 
 
+
     'componentDidMount': function () {
         this._parseDate = linechartUtils.parseDate;
     },
+
 
 
     'shouldComponentUpdate': function (nextProps, nextState) {
@@ -258,6 +279,7 @@ var MeasureByQuarterByGeographyVoronoiLineChart = React.createClass({
     },
 
 
+
     'componentDidUpdate': function (prevProps, prevState) {
         if (this.props.height !== prevProps.height) {
             linechartUtils.initByQuarterBasics.call(this);
@@ -265,6 +287,7 @@ var MeasureByQuarterByGeographyVoronoiLineChart = React.createClass({
 
         this._update();
     },
+
 
 
     '_getChartTitle': function () {
@@ -279,9 +302,10 @@ var MeasureByQuarterByGeographyVoronoiLineChart = React.createClass({
 
 
     '_savePng': function () {
-        var theSVG   = React.findDOMNode(this.refs.theSVG),
-            theClone = theSVG.cloneNode(true),
-            fileName = this._getChartTitle().replace(/\s+/g, '_');
+        var theSVG       = React.findDOMNode(this.refs.theSVG),
+            theClone     = theSVG.cloneNode(true),
+            fileName     = this._getChartTitle().replace(/\s+/g, '_'),
+            rightPadding = 5;
 
         // FIXME: There has to be a better way...
         // Gets the rightmost element in the svg tree to determine width.
@@ -293,7 +317,7 @@ var MeasureByQuarterByGeographyVoronoiLineChart = React.createClass({
                                                 .toArray());
 
         //Makes sure the exported image is complete, not cropped.
-        theClone.setAttribute('width', width + 5);
+        theClone.setAttribute('width', width + rightPadding);
         saveSvgAsPng(theClone, fileName);
     },
 
--- assets/react/d3/utils/linechart_utils.js
+++ assets/react/d3/utils/linechart_utils.js
@@ -11,10 +11,17 @@ module.exports = {
     'initByQuarterBasics' : function () {
 
         var theSVG = React.findDOMNode(this.refs.theSVG),
+            margin = this.props.margin,
             that   = this;
+
     
-        var width  = theSVG.offsetWidth - this.props.margin.right - this.props.margin.left,
-            height = this.props.height  - this.props.margin.top   - this.props.margin.bottom;
+        var width  = theSVG.offsetWidth - 
+                     margin.right - 
+                     margin.left,
+
+            height = this.props.height  - 
+                     margin.top - 
+                     margin.bottom;
 
 
         this._x = d3.time.scale().range([0, width]);
@@ -26,14 +33,17 @@ module.exports = {
                             .scale(this._x)
                             .orient("bottom")
                             .innerTickSize(-height)
-                            .tickPadding(this.props.margin.bottom / 2.0);
+                            .tickPadding(margin.bottom / 2.0);
 
 
-        this._yAxis = d3.svg.axis().scale(this._y).orient("left").innerTickSize(-width);
-
+        this._yAxis = d3.svg.axis()
+                            .scale(this._y)
+                            .orient("left")
+                            .innerTickSize(-width);
 
         this._line  = d3.svg.line()
-                    .x(function(d) { return that._x(d.date); })
-                    .y(function(d) { return that._y(d[that.props.measure]); });
+                            .x(function(d) { return that._x(d.date); })
+                            .y(function(d) { return that._y(d[that.props.measure]); });
     },
+
 };
